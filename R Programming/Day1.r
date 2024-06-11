e40171ed267448968533e2d03b24ca6205f57b1f
BDheerajChandan <1807265@kiit.ac.in>
2024-06-11 17:57:13 +0530

    Added R files named "Day1.r" and "Day2_Variables.r"

---
+++ R Programming/Day1.r
@@ -0,0 +1,12 @@
+# WAP to print "Hello World"
+"Hello World"
+5+5
+print("Hello World")
+print(5+5)
+# WAP to print numbers from 1 to 10
+# Using for loop
+for(variable in 1:10)
+{
+  print(variable)
+}
+
---
+++ R Programming/Day2_Variables.r
@@ -0,0 +1,21 @@
+# Variables assigning value
+a=10
+b="Dheeraj"
+c<-20
+d<-"Chandan"
+print(a)
+print(b)
+print(c)
+print(d)
+e<-f<-100
+g=h="Dheeraj"
+paste("Integer Value of e - ",e)
+paste("Integer Value of f - ",f)
+paste("String Value of g - ",g)
+paste("String Value of h - ",h)
+paste("Values of e,f,g,h = ",e,",",f,",",g,",",h)
+# Variable names
+a_1.r =1
+a_2 =2
+print(a_1.r)
+print(a_2)
